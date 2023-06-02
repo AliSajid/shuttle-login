4d2c2e3ae75eb6d571d55b6cbc1f360dbb561b1a
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-06-01 23:31:40 -0400

    test: update test env vars

---
+++ __tests__/test_setup.js
@@ -0,0 +1,4 @@
+"use strict";
+// setupTests.ts
+process.env.GITHUB_ACTIONS = 'false';
+process.env.ACTIONS_STEP_DEBUG = 'false';
--- __tests__/test_setup.ts
+++ __tests__/test_setup.ts
@@ -1,3 +1,3 @@
 // setupTests.ts
-process.env.GITHUB_ACTIONS = 'false';
-process.env.ACTIONS_STEP_DEBUG = 'false';
+process.env.GITHUB_ACTIONS = 'false'
+process.env.ACTIONS_STEP_DEBUG = 'false'
