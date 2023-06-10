efe64dc35dca787c3a4ca38785b76dd35291d1b6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-06-10 13:33:51 -0400

    build(pythonb): clear the directory for rebuilding in python

--- __tests__/test_setup.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// setupTests.ts
-process.env.GITHUB_ACTIONS = 'false';
-process.env.ACTIONS_STEP_DEBUG = 'false';
--- __tests__/test_setup.ts
+++
@@ -1,3 +0,0 @@
-// setupTests.ts
-process.env.GITHUB_ACTIONS = 'false'
-process.env.ACTIONS_STEP_DEBUG = 'false'
